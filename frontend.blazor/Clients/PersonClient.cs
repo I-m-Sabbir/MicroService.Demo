cdb17c4f036fc21b6e4ccd747ae2355027425376
sabbir_4730 <mdsabbir.onnorokom@gmail.com>
2023-03-01 12:06:40 +0600

    testing micro service

---
+++ frontend.blazor/Clients/PersonClient.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Json;
+
+namespace frontend.blazor.Clients
+{
+    public class PersonClient
+    {
+        private readonly JsonSerializerOptions _options = new JsonSerializerOptions()
+        {
+            PropertyNameCaseInsensitive = true,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        };
+
+        private readonly HttpClient _client;
+
+        public PersonClient(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<List<Person>> GetPeopleAsync()
+        {
+            try
+            {
+                var response = await _client.GetAsync("/PersonInfo");
+
+                if (response is not null)
+                {
+                    var stream = await response.Content.ReadAsStreamAsync();
+                    return await JsonSerializer.DeserializeAsync<List<Person>>(stream, _options);
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                throw ex;
+            }
+
+            return new List<Person>();
+        }
+    }
+}
